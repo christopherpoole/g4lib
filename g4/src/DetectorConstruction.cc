31814d6d7713f7adc06b7edc44ed5b192c7046d1
Christopher Poole <mail@christopherpoole.net>
2013-02-20 21:02:29 +1000

    added cadmesh for testing, wont run with python?

--- g4/src/DetectorConstruction.cc
+++ g4/src/DetectorConstruction.cc
@@ -26,8 +26,9 @@
 #include "globals.hh"
 #include "G4Material.hh"
 #include "G4NistManager.hh"
-
 #include "G4UserLimits.hh"
+
+#include "CADMesh.hh"
 
 DetectorConstruction::DetectorConstruction()
 {
@@ -40,6 +41,7 @@ DetectorConstruction::~DetectorConstruction()
 G4VPhysicalVolume* DetectorConstruction::Construct()
 {
     G4NistManager* nist_manager = G4NistManager::Instance();
+    G4Material* water = nist_manager->FindOrBuildMaterial("G4_WATER");
     G4Material* air = nist_manager->FindOrBuildMaterial("G4_AIR");
 
     world_solid = new G4Box("world_solid", 2*m, 2*m, 2*m);
@@ -47,6 +49,14 @@ G4VPhysicalVolume* DetectorConstruction::Construct()
     world_physical = new G4PVPlacement(0, G4ThreeVector(), world_logical, 
                                        "world_physical", 0, false, 0);
     
+    CADMesh * mesh = new CADMesh("../../models/cone.ply", "PLY", mm, G4ThreeVector(), false);
+
+    G4VSolid* cad_solid = mesh->TessellatedMesh();
+    G4LogicalVolume* cad_logical = new G4LogicalVolume(cad_solid, water,
+            "cad_logical", 0, 0, 0);
+    G4VPhysicalVolume* cad_physical = new G4PVPlacement(0, G4ThreeVector(), cad_logical,
+                                     "cad_physical", world_logical, false, 0);
+
     return world_physical;
 }
 
